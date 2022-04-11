4d9c2c1d8544b4ec38bd3c5e2d5d044ae3d4053c
Waqas Anjum <Qanjum96@gmail.com>
2022-04-11 11:17:40 -0400

    done

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+// a = 0, c = 4, 4 gets added into 0 making 4
+
+// a = 4, c = 5, 5 gets added into 4
+
+// a = 9, c = 3, 3 gets added into 9 
+
 // Code your solution here
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {
+     return accumulator + currentValue
+})
+
